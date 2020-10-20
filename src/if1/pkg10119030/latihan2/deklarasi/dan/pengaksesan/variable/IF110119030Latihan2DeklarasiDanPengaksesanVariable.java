c027771e485aea268590d94efe205abbad0c298b
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:18:13 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan2/deklarasi/dan/pengaksesan/variable/IF110119030Latihan2DeklarasiDanPengaksesanVariable.java
@@ -0,0 +1,29 @@
+package if1.pkg10119030.latihan2.deklarasi.dan.pengaksesan.variable;
+/**
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program : Program Latihan Mendeklarasikan Variable
+ */
+public class IF110119030Latihan2DeklarasiDanPengaksesanVariable {
+    public static void main(String[] args) {  
+        
+    //Deklarasi Variable
+    int nilaiInt;
+    final double PHI = 3.14; //Konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //Memberi nilai ke variable
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //Menampilkan Hasil
+    System.out.println();
+    System.out.println("Isi Variable nilai = " + nilaiInt);
+    System.out.println("Isi Variable PHI = " + PHI);
+    System.out.println("Isi Variable Logika = " + nilaiLogika);
+    System.out.println("Isi Variable Karakter = " + nilaiKarakter);  
+    }   
+}
